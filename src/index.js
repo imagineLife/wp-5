07d3bdbf610492fc19f6e2505ebd0c84e30d8ca3
Eric Laursen <ericjlaursen@gmail.com>
2021-02-15 19:34:32 -0500

    demo react component

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = () => <h1>Header Here </h1>
+
+ReactDOM.render(
+    <Header />,
+  document.getElementById("root")
+);
