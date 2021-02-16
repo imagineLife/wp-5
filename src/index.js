d20188ae7ae4c2779d69af5944b55953a1dedd31
Eric Laursen <ericjlaursen@gmail.com>
2021-02-15 19:44:42 -0500

    more

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 
-const Header = () => <p>Splitting Chunks: With the `optimization: splitChunks: chunks 'all'`, react gets split from the rest of the app. 56% of the react bundle goes unused.</p>
+const Header = () => <Fragment>
+<h1>Splitting Chunks</h1>
+<p>Splitting Chunks: With the `optimization: splitChunks: chunks 'all'`, react gets split from the rest of the app. 56% of the react bundle goes unused.</p>
+</Fragment>
 
 ReactDOM.render(
     <Header />,
