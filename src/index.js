38d15ac75c2109ee9f43834e7351e77de5b0cd61
Eric Laursen <ericjlaursen@gmail.com>
2022-07-02 21:12:13 -0400

    small change

--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ const Header = () => {
     <h1>Splitting Chunks</h1>
     <p>Splitting Chunks: With the `optimization: splitChunks: chunks 'all'`, react gets split from the rest of the app. 56% of the react bundle goes unused.</p>
 
-    <h1>Leveraging Fragmen</h1>
+    <h1>Leveraging Fragments</h1>
     <p>55% of the react bundle goes unused.</p>
 
     <p>Clicked: {clicked.toString()}</p>
 
-    <h1>SetState && Button Click</h1>
+    <h3>SetState && Button Click</h3>
     <p>With a button that is clickable && a state to update, the un-used react elements go down to 41% after clicking.</p>
 
     <button onClick={(e) => {
